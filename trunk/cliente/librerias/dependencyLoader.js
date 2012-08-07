968c95d5a829000ede4f89de293333d0cb1bcf5e
hbaizan@gmail.com <hbaizan@gmail.com@24766347-51ab-e813-d154-70da8487fbb9>
2012-08-07 18:08:01 +0000

    Jerarquia de librerias para cargar

---
+++ trunk/cliente/librerias/dependencyLoader.js
@@ -0,0 +1,32 @@
+Pyramid.rootPath = './cliente/';
+
+//Set up file dependencies
+Pyramid.newDependency({
+    name: 'standard',
+    files: [
+    'librerias/jquery-1.7.2.min.js',
+	 'librerias/knockout-2.1.0.js',
+	 'librerias/bootstrap.js',
+	 'librerias/bootstrap-tab.js',
+	 'librerias/bootstrap-collapse.js',
+	 'librerias/amplify.js'
+    ]
+});
+
+Pyramid.newDependency({
+name:'lookAndFeel',
+files: [
+	'estilos/bootstrap.css',
+	'estilos/bootstrap-responsive.css'
+    ]
+});
+
+Pyramid.newDependency({
+name:'main',
+files: [
+	'objetos/movies.js',
+	'objetos/actors.js',
+	'Index.js'
+   ],
+    dependencies: ['standard','lookAndFeel']
+});
