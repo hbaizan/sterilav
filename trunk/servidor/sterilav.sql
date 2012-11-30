66d47e8e25d43f8cbce204a496236ddd2d3653cb
hbaizan@gmail.com <hbaizan@gmail.com@24766347-51ab-e813-d154-70da8487fbb9>
2012-11-30 17:14:06 +0000

    Version Beta

--- trunk/servidor/sterilav.sql
+++ trunk/servidor/sterilav.sql
@@ -1,255 +1,19 @@
--- phpMyAdmin SQL Dump
--- version 3.5.2
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Sep 16, 2012 at 01:24 AM
--- Server version: 5.5.25a
--- PHP Version: 5.4.4
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
---
--- Database: `sterilav`
---
-
-CREATE TABLE iva (
-  idiva INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  iva_condicion VARCHAR() NULL,
-  PRIMARY KEY(idiva)
-);
-
-CREATE TABLE grupo (
-  idgrupo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  grupo_nombre VARCHAR NULL,
-  PRIMARY KEY(idgrupo)
-);
-
-CREATE TABLE perfil (
-  idperfil INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  perfil_nombre VARCHAR NULL,
-  PRIMARY KEY(idperfil)
-);
-
-CREATE TABLE provincia (
-  idprovincia INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  nombre VARCHAR NULL,
-  PRIMARY KEY(idprovincia)
-);
-
-CREATE TABLE remito_tipo (
-  idremito_tipo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  remito_tipo_nombre VARCHAR NULL,
-  PRIMARY KEY(idremito_tipo)
-);
-
-CREATE TABLE chofer (
-  idchofer INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  chofer_nombre VARCHAR NULL,
-  chofer_apellido VARCHAR NULL,
-  PRIMARY KEY(idchofer)
-);
-
-CREATE TABLE vehiculo (
-  idvehiculo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  vehiculo_patente VARCHAR NULL,
-  PRIMARY KEY(idvehiculo)
-);
-
-CREATE TABLE aplicaciones (
-  idaplicaciones INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  descripcion VARCHAR NULL,
-  link_aplicacion VARCHAR NULL,
-  PRIMARY KEY(idaplicaciones)
-);
-
-CREATE TABLE empresa (
-  idempresa INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  iva_idiva INTEGER UNSIGNED NOT NULL,
-  empresa_razon_social VARCHAR NULL,
-  empresa_cuit VARCHAR NULL,
-  PRIMARY KEY(idempresa),
-  INDEX empresa_FKIndex1(iva_idiva),
-  FOREIGN KEY(iva_idiva)
-    REFERENCES iva(idiva)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
-
-CREATE TABLE departamento (
-  iddepartamento INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  provincia_idprovincia INTEGER UNSIGNED NOT NULL,
-  departamento_nombre VARCHAR NULL,
-  PRIMARY KEY(iddepartamento),
-  INDEX departamento_FKIndex1(provincia_idprovincia),
-  FOREIGN KEY(provincia_idprovincia)
-    REFERENCES provincia(idprovincia)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
-
-CREATE TABLE Subgrupo (
-  idsubgrupo INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  grupo_idgrupo INTEGER UNSIGNED NOT NULL,
-  subgrupo_nombre VARCHAR(45) NULL,
-  PRIMARY KEY(idsubgrupo),
-  INDEX Subgrupo_FKIndex1(grupo_idgrupo),
-  FOREIGN KEY(grupo_idgrupo)
-    REFERENCES grupo(idgrupo)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
-
-CREATE TABLE permisos (
-  aplicaciones_idaplicaciones INTEGER UNSIGNED NOT NULL,
-  perfil_idperfil INTEGER UNSIGNED NOT NULL,
-  PRIMARY KEY(aplicaciones_idaplicaciones, perfil_idperfil),
-  INDEX aplicaciones_has_perfil_FKIndex1(aplicaciones_idaplicaciones),
-  INDEX aplicaciones_has_perfil_FKIndex2(perfil_idperfil),
-  FOREIGN KEY(aplicaciones_idaplicaciones)
-    REFERENCES aplicaciones(idaplicaciones)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(perfil_idperfil)
-    REFERENCES perfil(idperfil)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
-
-CREATE TABLE deposito (
-  iddeposito INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  departamento_iddepartamento INTEGER UNSIGNED NOT NULL,
-  empresa_idempresa INTEGER UNSIGNED NOT NULL,
-  deposito_nombre VARCHAR NULL,
-  deposito_domicilio VARCHAR NULL,
-  deposito_tiene_faltante BOOL NULL,
-  deposito_inactivo BOOL NULL,
-  PRIMARY KEY(iddeposito),
-  INDEX deposito_FKIndex1(empresa_idempresa),
-  INDEX deposito_FKIndex2(departamento_iddepartamento),
-  FOREIGN KEY(empresa_idempresa)
-    REFERENCES empresa(idempresa)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(departamento_iddepartamento)
-    REFERENCES departamento(iddepartamento)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
-
-CREATE TABLE persona (
-  idpersona INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  departamento_iddepartamento INTEGER UNSIGNED NOT NULL,
-  perfil_idperfil INTEGER UNSIGNED NOT NULL,
-  nombre VARCHAR NOT NULL,
-  apellido VARCHAR NULL,
-  usuario VARCHAR NULL,
-  clave VARCHAR NULL,
-  persona_inactivo BOOL NULL,
-  PRIMARY KEY(idpersona),
-  INDEX persona_FKIndex2(perfil_idperfil),
-  INDEX persona_FKIndex3(departamento_iddepartamento),
-  FOREIGN KEY(perfil_idperfil)
-    REFERENCES perfil(idperfil)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(departamento_iddepartamento)
-    REFERENCES departamento(iddepartamento)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
-
-CREATE TABLE remito (
-  idremito INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  persona_idpersona INTEGER UNSIGNED NOT NULL,
-  deposito_iddeposito INTEGER UNSIGNED NOT NULL,
-  chofer_idchofer INTEGER UNSIGNED NOT NULL,
-  remito_tipo_idremito_tipo INTEGER UNSIGNED NOT NULL,
-  vehiculo_idvehiculo INTEGER UNSIGNED NOT NULL,
-  remito_fecha DATE NULL,
-  remito_hora_entrega DATE NULL,
-  remito_hora_retiro DATE NULL,
-  remito_observaciones VARCHAR NULL,
-  PRIMARY KEY(idremito),
-  INDEX remito_FKIndex1(vehiculo_idvehiculo),
-  INDEX remito_FKIndex2(remito_tipo_idremito_tipo),
-  INDEX remito_FKIndex3(chofer_idchofer),
-  INDEX remito_FKIndex4(deposito_iddeposito),
-  INDEX remito_FKIndex5(persona_idpersona),
-  FOREIGN KEY(vehiculo_idvehiculo)
-    REFERENCES vehiculo(idvehiculo)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(remito_tipo_idremito_tipo)
-    REFERENCES remito_tipo(idremito_tipo)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(chofer_idchofer)
-    REFERENCES chofer(idchofer)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(deposito_iddeposito)
-    REFERENCES deposito(iddeposito)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(persona_idpersona)
-    REFERENCES persona(idpersona)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
-
-CREATE TABLE producto (
-  idproducto INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  Subgrupo_idsubgrupo INTEGER UNSIGNED NOT NULL,
-  producto_nombre VARCHAR NULL,
-  PRIMARY KEY(idproducto),
-  INDEX producto_FKIndex1(Subgrupo_idsubgrupo),
-  FOREIGN KEY(Subgrupo_idsubgrupo)
-    REFERENCES Subgrupo(idsubgrupo)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
-
-CREATE TABLE remito_has_producto (
-  remito_idremito INTEGER UNSIGNED NOT NULL,
-  producto_idproducto INTEGER UNSIGNED NOT NULL,
-  cantidad_real INTEGER UNSIGNED NULL,
-  cantidad_faltante INTEGER UNSIGNED NULL,
-  PRIMARY KEY(remito_idremito, producto_idproducto),
-  INDEX remito_has_producto_FKIndex1(remito_idremito),
-  INDEX remito_has_producto_FKIndex2(producto_idproducto),
-  FOREIGN KEY(remito_idremito)
-    REFERENCES remito(idremito)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(producto_idproducto)
-    REFERENCES producto(idproducto)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
-
-CREATE TABLE producto_has_deposito (
-  producto_idproducto INTEGER UNSIGNED NOT NULL,
-  deposito_iddeposito INTEGER UNSIGNED NOT NULL,
-  cantidad_tope INTEGER UNSIGNED NULL,
-  deposito_capita INTEGER UNSIGNED NULL,
-  stock_inicial INTEGER UNSIGNED NULL,
-  PRIMARY KEY(producto_idproducto, deposito_iddeposito),
-  INDEX producto_has_deposito_FKIndex1(producto_idproducto),
-  INDEX producto_has_deposito_FKIndex2(deposito_iddeposito),
-  FOREIGN KEY(producto_idproducto)
-    REFERENCES producto(idproducto)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(deposito_iddeposito)
-    REFERENCES deposito(iddeposito)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `aplicacion` (
+  `idaplicacion` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(50) DEFAULT NULL,
+  `link_aplicacion` varchar(50) DEFAULT NULL,
+  `grupo_idgrupopantalla` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`idaplicacion`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=24 ;
 
 INSERT INTO `aplicacion` (`idaplicacion`, `descripcion`, `link_aplicacion`, `grupo_idgrupopantalla`) VALUES
 (1, 'Personas', 'usuario', 1),
@@ -268,48 +32,88 @@ INSERT INTO `aplicacion` (`idaplicacion`, `descripcion`, `link_aplicacion`, `gru
 (14, 'Vehiculo', 'vehiculo', 2),
 (15, 'Lista de Vehiculos', 'listaVehiculos', 2),
 (16, 'Lista de Choferes', 'listaChoferes', 1),
-(17, 'Subgrupo', 'subgrupo', 3),
-(18, 'Lista de Subgrupos', 'listaSubgrupos', 3);
+(18, 'Subgrupo', 'subgrupo', 3),
+(19, 'Lista de Subgrupos', 'listaSubgrupos', 3),
+(20, 'Productos por Cliente', 'productosPorCliente', 5),
+(21, 'Remitos por Chofer', 'remitosPorChofer', 5),
+(22, 'Productos por Grupo', 'productosPorGrupo', 5),
+(23, 'Clientes por Capita', 'clientesPorCapita', 5);
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `chofer` (
+  `idchofer` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `chofer_nombre` varchar(20) DEFAULT NULL,
+  `chofer_apellido` varchar(20) DEFAULT NULL,
+  `chofer_inactivo` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`idchofer`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+CREATE TABLE IF NOT EXISTS `departamento` (
+  `iddepartamento` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `provincia_idprovincia` int(10) unsigned NOT NULL,
+  `departamento_nombre` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`iddepartamento`),
+  KEY `departamento_FKIndex1` (`provincia_idprovincia`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 INSERT INTO `departamento` (`iddepartamento`, `provincia_idprovincia`, `departamento_nombre`) VALUES
 (1, 1, 'Guaymallen'),
-(2, 1, 'Godoy Cruz'),
-(3, 1, 'Mendoza'),
-(4, 1, 'Luján de Cuyo'),
-(5, 1, 'Maipú'),
-(6, 1, 'San Martín'),
-(7, 1, 'Junín'),
-(8, 1, 'San Rafael'),
-(9, 1, 'Gral. Alvear'),
-(10, 1, 'Malargüe');
+(2, 1, 'Godoy Cruz');
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `deposito` (
+  `iddeposito` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `departamento_iddepartamento` int(10) unsigned NOT NULL,
+  `empresa_idempresa` int(10) unsigned NOT NULL,
+  `deposito_nombre` varchar(50) DEFAULT NULL,
+  `deposito_domicilio` varchar(50) DEFAULT NULL,
+  `deposito_inactivo` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`iddeposito`),
+  KEY `deposito_FKIndex1` (`empresa_idempresa`),
+  KEY `deposito_FKIndex2` (`departamento_iddepartamento`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
-INSERT INTO `grupo` (`idgrupo`, `grupo_nombre`) VALUES
-(1, 'QUIROFANO');
+CREATE TABLE IF NOT EXISTS `empresa` (
+  `idempresa` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `iva_idiva` int(10) unsigned NOT NULL,
+  `empresa_razon_social` varchar(50) DEFAULT NULL,
+  `empresa_cuit` varchar(11) DEFAULT NULL,
+  PRIMARY KEY (`idempresa`),
+  KEY `empresa_FKIndex1` (`iva_idiva`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `grupo` (
+  `idgrupo` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `grupo_nombre` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`idgrupo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
-INSERT INTO `subgrupo` (`idsubgrupo`, `subgrupo_nombre`, `grupo_idgrupo`) VALUES
-(1, 'Packs', 1);
-
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `grupopantalla` (
+  `idgrupopantalla` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`idgrupopantalla`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 INSERT INTO `grupopantalla` (`idgrupopantalla`, `nombre`) VALUES
 (1, 'Personas'),
 (2, 'Clientes'),
 (3, 'Productos'),
-(4, 'Administracion');
+(4, 'Administracion'),
+(5, 'Reportes');
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `iva` (
+  `idiva` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `iva_condicion` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`idiva`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 INSERT INTO `iva` (`idiva`, `iva_condicion`) VALUES
 (1, 'Inscripto'),
 (2, 'No Inscripto');
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `perfil` (
+  `idperfil` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `perfil_nombre` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`idperfil`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 INSERT INTO `perfil` (`idperfil`, `perfil_nombre`) VALUES
 (1, 'Administrador'),
@@ -317,7 +121,13 @@ INSERT INTO `perfil` (`idperfil`, `perfil_nombre`) VALUES
 (3, 'Contador'),
 (4, 'Cliente');
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `permiso` (
+  `aplicacion_idaplicacion` int(10) unsigned NOT NULL,
+  `perfil_idperfil` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`aplicacion_idaplicacion`,`perfil_idperfil`),
+  KEY `aplicacion_has_perfil_FKIndex1` (`aplicacion_idaplicacion`),
+  KEY `aplicacion_has_perfil_FKIndex2` (`perfil_idperfil`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `permiso` (`aplicacion_idaplicacion`, `perfil_idperfil`) VALUES
 (1, 1),
@@ -341,44 +151,94 @@ INSERT INTO `permiso` (`aplicacion_idaplicacion`, `perfil_idperfil`) VALUES
 (15, 1),
 (16, 1),
 (17, 1),
-(18, 1);
+(18, 1),
+(19, 1),
+(20, 1),
+(21, 1),
+(22, 1),
+(23, 1);
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `persona` (
+  `idpersona` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `departamento_iddepartamento` int(10) unsigned NOT NULL,
+  `perfil_idperfil` int(10) unsigned NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `apellido` varchar(20) DEFAULT NULL,
+  `usuario` varchar(20) DEFAULT NULL,
+  `clave` varchar(20) DEFAULT NULL,
+  `persona_inactivo` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`idpersona`),
+  KEY `persona_FKIndex2` (`perfil_idperfil`),
+  KEY `persona_FKIndex3` (`departamento_iddepartamento`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 INSERT INTO `persona` (`idpersona`, `departamento_iddepartamento`, `perfil_idperfil`, `nombre`, `apellido`, `usuario`, `clave`, `persona_inactivo`) VALUES
 (1, 1, 1, 'sterilav', 'admin', 'sterilav', 'sterilav', 0);
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `producto` (
+  `idproducto` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `subgrupo_idsubgrupo` int(10) unsigned NOT NULL,
+  `producto_nombre` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`idproducto`),
+  KEY `producto_FKIndex1` (`subgrupo_idsubgrupo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
 
-INSERT INTO `producto` (`idproducto`, `subgrupo_idsubgrupo`, `producto_nombre`) VALUES
-(1, 1, 'Pack cirugia grande '),
-(2, 1, 'Pack cirugia mediano'),
-(3, 1, 'Pack cirugia chico e'),
-(4, 1, 'Pack parto'),
-(5, 1, 'Pack cesarea'),
-(6, 1, 'Pack cirugia descart'),
-(7, 1, 'Pack anestesia ester'),
-(8, 1, 'Blusones cirugia est'),
-(9, 1, 'Campo grande esteril'),
-(10, 1, 'Campo chico esteril'),
-(11, 1, 'Campo grande fenstra'),
-(12, 1, 'Campo chico fenestra'),
-(13, 1, 'Pantalones'),
-(14, 1, 'Chaquetas'),
-(15, 1, 'Ambos de cirugia'),
-(16, 1, 'Bata paciente'),
-(17, 1, 'Pierneras'),
-(18, 1, 'Guardapolvos'),
-(19, 1, 'Camilleros'),
-(20, 1, 'Atapaquetes');
+CREATE TABLE IF NOT EXISTS `producto_has_deposito` (
+  `producto_idproducto` int(10) unsigned NOT NULL,
+  `deposito_iddeposito` int(10) unsigned NOT NULL,
+  `cantidad_tope` int(10) unsigned DEFAULT NULL,
+  `deposito_capita` int(10) unsigned DEFAULT NULL,
+  `stock_inicial` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`producto_idproducto`,`deposito_iddeposito`),
+  KEY `producto_has_deposito_FKIndex1` (`producto_idproducto`),
+  KEY `producto_has_deposito_FKIndex2` (`deposito_iddeposito`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `provincia` (
+  `idprovincia` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`idprovincia`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 INSERT INTO `provincia` (`idprovincia`, `nombre`) VALUES
 (1, 'Mendoza'),
 (2, 'San Juan');
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `remito` (
+  `idremito` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `persona_idpersona` int(10) unsigned NOT NULL,
+  `deposito_iddeposito` int(10) unsigned NOT NULL,
+  `chofer_idchofer` int(10) unsigned NOT NULL,
+  `remito_tipo_idremito_tipo` int(10) unsigned NOT NULL,
+  `vehiculo_idvehiculo` int(10) unsigned NOT NULL,
+  `remito_fecha` date DEFAULT NULL,
+  `remito_hora_entrega` time DEFAULT NULL,
+  `remito_hora_retiro` time DEFAULT NULL,
+  `remito_observaciones` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idremito`),
+  KEY `remito_FKIndex1` (`vehiculo_idvehiculo`),
+  KEY `remito_FKIndex2` (`remito_tipo_idremito_tipo`),
+  KEY `remito_FKIndex3` (`chofer_idchofer`),
+  KEY `remito_FKIndex4` (`deposito_iddeposito`),
+  KEY `remito_FKIndex5` (`persona_idpersona`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=62 ;
+
+CREATE TABLE IF NOT EXISTS `remito_has_producto` (
+  `remito_idremito` int(10) unsigned NOT NULL,
+  `producto_idproducto` int(10) unsigned NOT NULL,
+  `ingreso` int(10) unsigned DEFAULT NULL,
+  `cantidad_faltante` int(10) unsigned DEFAULT NULL,
+  `egreso` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`remito_idremito`,`producto_idproducto`),
+  KEY `remito_has_producto_FKIndex1` (`remito_idremito`),
+  KEY `remito_has_producto_FKIndex2` (`producto_idproducto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `remito_tipo` (
+  `idremito_tipo` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `remito_tipo_nombre` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`idremito_tipo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 INSERT INTO `remito_tipo` (`idremito_tipo`, `remito_tipo_nombre`) VALUES
 (1, 'Remito de Entrada'),
@@ -387,4 +247,24 @@ INSERT INTO `remito_tipo` (`idremito_tipo`, `remito_tipo_nombre`) VALUES
 (4, 'Ajuste'),
 (5, 'Recomposicion');
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `subgrupo` (
+  `idsubgrupo` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `grupo_idgrupo` int(10) unsigned NOT NULL,
+  `subgrupo_nombre` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idsubgrupo`),
+  KEY `Subgrupo_FKIndex1` (`grupo_idgrupo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+CREATE TABLE IF NOT EXISTS `vehiculo` (
+  `idvehiculo` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `vehiculo_patente` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`idvehiculo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+
+ALTER TABLE `subgrupo`
+  ADD CONSTRAINT `subgrupo_ibfk_1` FOREIGN KEY (`grupo_idgrupo`) REFERENCES `grupo` (`idgrupo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
